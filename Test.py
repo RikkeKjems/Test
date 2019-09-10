43ad144ceafffaed84426690421af7705213c26d
Rikke Kjems <rikkekjems@Rikkes-MacBook-Air.local>
2019-09-10 12:47:53 +0200

    Exercises from Kenneth

--- Test.py
+++ Test.py
@@ -1,1 +1,37 @@
 d = 2 +2
+
+print("d is " + str(d))
+
+type(d)
+print(f"d is {d} which is number of type {type(d)}")
+
+string = "this is a string"
+
+string.split(" ")
+
+a_list_a_strings = string.split(" ")
+a_list_a_strings[0]
+
+def print_hello():
+    print("hello world")
+
+#making a new equal function
+new_name = print_hello
+
+def add_hello_to_string(string):
+    return string + "hello"
+
+string = "this is Westworld"
+
+string + ", yes it is"
+
+string *3
+
+string.split(" ")
+
+string.title()
+
+d = 2 + 3
+
+print(d)
+print("2 + 3 = " + str(d))
